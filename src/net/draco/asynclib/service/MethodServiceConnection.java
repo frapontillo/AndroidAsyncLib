3f78902d6e467936cfaca4e0d364c930cd6917d4
Sebastiano Poggi <poggos@gmail.com>
2012-12-12 12:35:40 +0100

    Added IDEA v12 project, changed target SDK to 17 (4.2), minor stuff

--- src/net/draco/asynclib/service/MethodServiceConnection.java
+++ src/net/draco/asynclib/service/MethodServiceConnection.java
@@ -7,40 +7,40 @@ import android.util.Log;
 
 /**
  * ServiceConnection implementation for handling a Service.
- * 
- * @author Francesco Pontillo
  *
+ * @author Francesco Pontillo
  */
 public class MethodServiceConnection<T extends ServiceBinder> implements ServiceConnection {
-	public T serviceBinder = null;
-	private boolean isConnected = false;
-	private IServiceConnectionListener mListener = null;
-	
-	public MethodServiceConnection(IServiceConnectionListener listener) {
-		this.mListener = listener;
-	}
-	
-	@Override
-	public void onServiceConnected(ComponentName className, IBinder rawBinder) {
-		serviceBinder = (T)rawBinder;
-		isConnected = true;
-		Log.i("MethodServiceConnection", "Service connected!");
-		// Calls the connection callback
-		if (mListener != null) mListener.onServiceConnected();
-		// Random code for overridden method service connections
-		performOnConnection();
-	}
+    public T serviceBinder = null;
+    private boolean isConnected = false;
+    private IServiceConnectionListener mListener = null;
 
-	@Override
-	public void onServiceDisconnected(ComponentName className) {
-		Log.i("MethodServiceConnection", "Service disconnected!");
-		isConnected = false;
-		serviceBinder = null;
-	}
-	
-	public boolean isConnected() {
-		return isConnected;
-	}
-	
-	public void performOnConnection() {};
+    public MethodServiceConnection(IServiceConnectionListener listener) {
+        this.mListener = listener;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+        serviceBinder = (T) rawBinder;
+        isConnected = true;
+        Log.i("MethodServiceConnection", "Service connected!");
+        // Calls the connection callback
+        if (mListener != null) mListener.onServiceConnected();
+        // Random code for overridden method service connections
+        performOnConnection();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName className) {
+        Log.i("MethodServiceConnection", "Service disconnected!");
+        isConnected = false;
+        serviceBinder = null;
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void performOnConnection() {}
 }
